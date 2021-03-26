1d869a0f276400dbb52f3544366472315268eb53
julianasaavedra <57683111+julianasaavedra@users.noreply.github.com>
2021-03-25 21:48:58 -0400

    Rendered the definitions and examples of the entered word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,25 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css"
 
 export default function Dictionary(){
     let [keyword, setKeyword]=useState(null);
+    let [definition, setDefinition]=useState(null);
 
     function handleResult(response){
         console.log(response.data[0]);
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setDefinition(response.data[0]);
 
     }
     
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResult);
     }
-
-    
-
+ 
     function handleChange(event){
         setKeyword(event.target.value);
     }
@@ -29,6 +30,7 @@ export default function Dictionary(){
                 <input type="search" placeholder="Type a word" autoFocus={true} onChange={handleChange}/>
                 <input type="submit" className="btn btn search" />
             </form>
+            <Results results={definition}/>
         </div>
     ) 
 
